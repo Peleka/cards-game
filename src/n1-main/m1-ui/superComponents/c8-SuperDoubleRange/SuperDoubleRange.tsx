39b1ed160325fbf57750d636b2f4ce2c97f36b31
Peleka <ego.iriska@gmail.com>
2021-08-20 17:33:53 +0300

    add super components

---
+++ src/n1-main/m1-ui/superComponents/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {Slider} from "@material-ui/core";
+
+type SuperDoubleRangePropsType = {
+    onChangeRange?:  (event: any, value: number | number[]) => void
+    value?: [number, number]
+    // min, max, step, disable, ...
+}
+
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
+    {
+        onChangeRange, value,
+        // min, max, step, disable, ...
+    }
+) => {
+    // сделать самому, можно подключать библиотеки
+
+    return (
+        <>
+            <Slider
+                style={{margin: '0 10px', height: '10px'}}
+                value={value}
+                onChange={onChangeRange}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider"
+            />
+        </>
+    );
+}
+
+export default SuperDoubleRange;
