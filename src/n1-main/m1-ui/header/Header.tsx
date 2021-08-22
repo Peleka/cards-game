f8703d4204dbe303858d930035f3a3a06bfc42be
aleksandrakorelskaia <korelskaia@gmail.com>
2021-08-22 23:31:37 +0300

    aleks Login Log out

--- src/n1-main/m1-ui/header/Header.tsx
+++ src/n1-main/m1-ui/header/Header.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import s from './Header.module.css'
 import { NavLink } from "react-router-dom";
 import {PATH} from "../routes/Routes";
+import SuperButton from "../superComponents/c2-SuperButton/SuperButton";
+import {useDispatch, useSelector} from "react-redux";
+import {logoutTC} from "../../m2-bll/authReducer";
+import {AppStoreType} from "../../m2-bll/store";
 
 export const Header = () => {
+
+    const dispatch = useDispatch()
+    const isLoggedIn = useSelector((state: AppStoreType) => state.auth.isLoggedIn)
+
     return (
         <div>
             <div className={s.wrapper}>
@@ -11,6 +19,8 @@ export const Header = () => {
                 <NavLink to={PATH.TEST} activeClassName={s.active}>Test</NavLink>
                 <NavLink to={PATH.REGISTER} activeClassName={s.active}>Register</NavLink>
                 <NavLink to={PATH.PROFILE} activeClassName={s.active}>Profile</NavLink>
+                <SuperButton onClick={() => {dispatch(logoutTC)
+                    console.log(isLoggedIn)}}>Log out</SuperButton>
             </div>
         </div>
     )
