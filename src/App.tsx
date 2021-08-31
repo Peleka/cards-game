60f7915d6f314ee053af9db658fda07d8b7d49cc
aleksandrakorelskaia <korelskaia@gmail.com>
2021-08-31 17:24:52 +0300

    custom loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Main} from "./n1-main/Main";
-import {CircularProgress} from "@material-ui/core";
 import {useSelector} from "react-redux";
 import {AppStoreType} from "./n1-main/m2-bll/store";
 import {RequestStatusType} from "./n1-main/m2-bll/app-reducer";
+import {Loader} from "./n1-main/m1-ui/superComponents/Loader/Loader";
 
 const App = () => {
     const status = useSelector<AppStoreType, RequestStatusType>(state => state.app.status)
 
   return (
     <div className="App">
-        {status === 'loading' && <CircularProgress/>}
+        {status === 'loading' && <Loader />}
       <Main/>
     </div>
   );
